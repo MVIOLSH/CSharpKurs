82ca3f813ba3d47dfa5b22843055e571646ef862
Michał Szymański <mviolsh@gmail.com>
2018-11-29 02:09:01 +0000

    Dodaj pliki projektów.

---
+++ StudentDiary/Diary.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDiary
+{
+    class Diary
+    {
+        //Stan(Zmienne - pola)
+        List<float> ratings;
+
+
+        //Zachowania
+        public void AddRating(float rating)
+        {
+            ratings.Add(rating);
+        }
+
+
+        //Obliczanie 
+        public float CalculateAverage()
+        {
+            float sum = 0, avg = 0;
+                       
+            foreach (var rating in ratings)
+            {
+                sum += rating;
+
+            }
+
+            avg = sum / ratings.Count();
+
+            return avg;
+        }
+
+
+        //Zwracanie najwyższej wartości
+        public float GivemaxRating()
+        {
+            return ratings.Max();
+        }
+
+
+        //Zwracanie najniższej wartości
+        public float GiveMinRating()
+        {
+            return ratings.Min();
+        }
+    }
+}
